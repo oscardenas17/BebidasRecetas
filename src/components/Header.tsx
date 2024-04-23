a0fdea0b9ae75fe0f791e6f850d0cca9e0783e19
oscardenas <oscardenas17@gmail.com>
2024-04-22 19:37:33 -0500

    useLocation par ruta actual y mostrar el formulario en la ruta indicada

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,10 @@
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
 const Header = () => {
+  const location = useLocation();
+
+  const isHome = useMemo(() => location.pathname, [location.pathname]);
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
@@ -7,8 +13,74 @@ const Header = () => {
             {" "}
             <img className="w-32" src="/logo.svg" alt="logotipo" />
           </div>
-          <nav></nav>
+          <nav className="flex gap-4">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              to={"/favoritos"}
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Favoritos
+            </NavLink>
+          </nav>
         </div>
+
+        {isHome && (
+          <form
+            action=""
+            className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-16 p-10 rounded-lg shadow space-y-6 "
+          >
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o ingredientes
+              </label>
+              <input
+                type="text"
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Café"
+              />
+            </div>
+
+            <div className="space-y-4">
+              <label
+                htmlFor="category"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoría{" "}
+              </label>
+              <select
+                id="category"
+                name="category"
+                className="p-3 w-full rounded-lg focus:outline-none"
+              >
+                <option value=""> -- Seleccione --</option>
+              </select>
+            </div>
+
+            <input
+              type="submit"
+              value={"Buscar Recetas"}
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
